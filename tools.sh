09b6072b05fd61f429d40aceab2eef6eb5c7ba91
issamiso <101994309+issamiso@users.noreply.github.com>
2022-11-14 19:57:41 +0100

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,36 @@
+pkg install figlet
+clear
+red='\033[1;31m'
+echo -e "$red"
+figlet I S O
+sleep 2
+green='\033[1;32m'
+echo -e "$green" 
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install ruby -y
+pkg install git -y
+pkg install php -y
+pkg install java -y
+pkg install google -y
+pkg install bash -y
+pkg install perl -y
+pkg install nmap -y
+pkg install clang -y
+pkg install macchanger -y
+pkg install nano -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wcalc -y
+pkg install openssl -y
+pkg install bmon -y
+
